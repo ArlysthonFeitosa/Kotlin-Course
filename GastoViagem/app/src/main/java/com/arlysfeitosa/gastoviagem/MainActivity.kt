c759e793876dc77c699320554026fd403fe36a3d
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-11-19 10:13:08 -0300

    button calculate action

--- GastoViagem/app/src/main/java/com/arlysfeitosa/gastoviagem/MainActivity.kt
+++ GastoViagem/app/src/main/java/com/arlysfeitosa/gastoviagem/MainActivity.kt
@@ -2,12 +2,42 @@ package com.arlysfeitosa.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonCalculate
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        if (validationOk()) {
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance + price) / autonomy
+            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(applicationContext, getString(R.string.digite_todos_os_valores), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validationOk(): Boolean {
+        return (editDistance.text.toString() != ""
+                && editPrice.text.toString() != ""
+                && editAutonomy.text.toString() != "")
     }
 }
