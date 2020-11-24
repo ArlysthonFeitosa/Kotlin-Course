eb7610d3498f2644ce53d8d5a1083405cc2e504b
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-11-24 15:34:34 -0300

    Create funcoesInfix.kt

---
+++ MaisFuncoes/src/FuncoesInfix/funcoesInfix.kt
@@ -0,0 +1,10 @@
+package FuncoesInfix
+
+//quando a função recebe um parâmetro pode usar infix para escrever melhor a fun
+infix fun Int.isHalfOf(value:Int) = value/2 == this
+
+
+fun main(args: Array<String>){
+    10.isHalfOf(20)
+    10 isHalfOf 20 //por causa do infix pode escrever assim
+}
