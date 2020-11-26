a134b28db7217345d2b0405309b9be19700f0921
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-11-26 08:58:25 -0300

    tirando actionbar

--- Motivation/app/src/main/java/com/arlysfeitosa/motivation/SplashActivity.kt
+++ Motivation/app/src/main/java/com/arlysfeitosa/motivation/SplashActivity.kt
@@ -7,5 +7,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        if(supportActionBar != null){
+            supportActionBar!!.hide()
+        }
     }
 }
