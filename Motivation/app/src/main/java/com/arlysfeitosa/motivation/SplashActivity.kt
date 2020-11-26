d962c2a645b6ba325c3555ab5c3228f8dc1324b4
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-11-26 09:27:44 -0300

    eventos de  click splash activity

--- Motivation/app/src/main/java/com/arlysfeitosa/motivation/SplashActivity.kt
+++ Motivation/app/src/main/java/com/arlysfeitosa/motivation/SplashActivity.kt
@@ -1,15 +1,38 @@
 package com.arlysfeitosa.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        if(supportActionBar != null){
+        if (supportActionBar != null) {
             supportActionBar!!.hide()
+        }
+
+        buttonSave.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v!!.id == R.id.buttonSave) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = editName.text.toString()
+
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(applicationContext, "Informe seu nome", Toast.LENGTH_SHORT).show()
         }
     }
 }
