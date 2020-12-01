b926d6bcee64a152136b1726af64ee96c325c399
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-12-01 15:57:08 -0300

    exercicio cappuccino

--- ConstraintLayout/app/src/main/java/com/arlysfeitosa/constraintlayout/CoffeExercicio.kt
+++ ConstraintLayout/app/src/main/java/com/arlysfeitosa/constraintlayout/CoffeExercicio.kt
@@ -2,10 +2,15 @@ package com.arlysfeitosa.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class CoffeExercicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffe_exercicio)
+
+
+        if(supportActionBar != null) supportActionBar!!.hide()
+
     }
 }
