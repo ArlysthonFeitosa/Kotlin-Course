90029680391a568811260a7f7c719e1f88f36175
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-12-03 08:39:15 -0300

    GuestForm ViewModel

--- Convidados/app/src/main/java/com/arlysfeitosa/convidados/view/GuestFormActivity.kt
+++ Convidados/app/src/main/java/com/arlysfeitosa/convidados/view/GuestFormActivity.kt
@@ -1,8 +1,11 @@
 package com.arlysfeitosa.convidados.view
 
+import android.content.ComponentCallbacks2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.arlysfeitosa.convidados.viewmodel.GuestFormViewModel
 import com.arlysfeitosa.convidados.R
@@ -19,16 +22,30 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
 
         setListeners()
-    }
 
-    private fun setListeners() {
-        button_save.setOnClickListener(this)
+        observe()
     }
 
     override fun onClick(v: View) {
         val id = v.id
         if (id == R.id.button_save) {
-
+            val name: String = edit_name.text.toString()
+            val presence = radio_presence.isChecked()
+            mViewModel.save(name, presence)
         }
+    }
+
+    private fun observe() {
+        mViewModel.saveGuest.observe(this, Observer {
+            if (it) {
+                Toast.makeText(applicationContext, "Sucesso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Falha", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun setListeners() {
+        button_save.setOnClickListener(this)
     }
 }
--- Convidados/app/src/main/java/com/arlysfeitosa/convidados/viewmodel/GuestFormViewModel.kt
+++ Convidados/app/src/main/java/com/arlysfeitosa/convidados/viewmodel/GuestFormViewModel.kt
@@ -1,6 +1,15 @@
 package com.arlysfeitosa.convidados.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GuestFormViewModel : ViewModel() {
+
+    private var mSaveGuest = MutableLiveData<Boolean>()
+    val saveGuest:LiveData<Boolean> = mSaveGuest
+
+    fun save(name: String, presence: Boolean) {
+
+    }
 }
