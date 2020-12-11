560ed26655b31f89474020e1fa4718736a26ac03
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-12-11 10:46:46 -0300

    GuestDataBase Room

--- Convidados/app/src/main/java/com/arlysfeitosa/convidados/service/repository/GuestDataBase.kt
+++ Convidados/app/src/main/java/com/arlysfeitosa/convidados/service/repository/GuestDataBase.kt
@@ -1,7 +1,21 @@
 package com.arlysfeitosa.convidados.service.repository
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-abstract class GuestDataBase : RoomDatabase() {
-    
+abstract class GuestDataBase() : RoomDatabase() {
+
+    companion object {
+        private lateinit var INSTANCE: GuestDataBase
+
+        fun getDataBase(context: Context): GuestDataBase {
+            if (!::INSTANCE.isInitialized) {
+                INSTANCE = Room.databaseBuilder(context, GuestDataBase::class.java, "guestDB")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE
+        }
+    }
 }
