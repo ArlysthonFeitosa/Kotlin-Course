743ab0380179f3305bb37119ef3920ee2d39d945
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2020-12-16 10:54:14 -0300

    obtenção de valores

--- TasksTemplate/app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.DatePicker
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
+import com.example.tasks.service.model.TaskModel
 import com.example.tasks.viewmodel.TaskFormViewModel
 import kotlinx.android.synthetic.main.activity_register.button_save
 import kotlinx.android.synthetic.main.activity_task_form.*
@@ -18,6 +19,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDi
 
     private lateinit var mViewModel: TaskFormViewModel
     private val mDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    private val mListPriorityId: MutableList<Int> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +37,22 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDi
     override fun onClick(v: View) {
         val id = v.id
         if (id == R.id.button_save) {
-
+            handleSave()
 
         } else if(id == R.id.button_date){
             showDatePicker()
         }
+    }
+
+    private fun handleSave(){
+        val Task:TaskModel = TaskModel().apply {
+            this.description = edit_description.text.toString()
+            this.dueDate = button_date.text.toString()
+            this.complete = check_complete.isChecked
+            this.priorityId = mListPriorityId[spinner_priority.selectedItemPosition]
+        }
+
+        mViewModel.save(Task)
     }
 
     private fun showDatePicker(){
@@ -61,13 +74,12 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDi
     private fun observe() {
         mViewModel.priorities.observe(this, androidx.lifecycle.Observer {
             val list: MutableList<String> = arrayListOf()
-
             for(item in it){
                 list.add(item.description)
+                mListPriorityId.add(item.id)
             }
 
             val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
-
             spinner_priority.adapter = adapter
 
         })
--- TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
+++ TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.tasks.service.listener.ValidationListener
 import com.example.tasks.service.model.PriorityModel
+import com.example.tasks.service.model.TaskModel
 import com.example.tasks.service.repository.PriorityRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
@@ -17,5 +18,9 @@ class TaskFormViewModel(application: Application) : AndroidViewModel(application
 
     fun listPriorities(){
         mpriorityList.value = mPriorityRepository.list()
+    }
+
+    fun save(task: TaskModel){
+
     }
 }
